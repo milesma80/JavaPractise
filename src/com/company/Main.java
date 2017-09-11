036266247dde817edffc1560ac8797f4af1d3acc
milesma80 <ma.hongwei@gmail.com>
2017-09-11 22:05:34 +1000

    created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class HelloWorld {
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+	    System.out.println("Hello World!");
     }
 }
